ea541b2ea4de00f2dd03b5c942bd87ba5a8668a1
Michael Montgomery <mem22695@gmail.com>
2021-06-06 22:00:59 -0400

    worked on editing feature

--- edit.py
+++ edit.py
@@ -30,32 +30,62 @@ def edit_character():
 
     character = characters[choice]
     questions = character.questions
-    incrementer = 0
-    incrementer2 = 1
-    inc = 1
-    for i in questions:
-        print(f'Q{incrementer + 1}: {questions[incrementer].body}')
-        for j in i.answers:
-            print(f'{inc}. {j.body} ---> {j.reward}')
-            inc = inc + 1
-        incrementer = incrementer + 1
-        print()
-        print('-' * 40)
-        print()
-    while True:
-        try:
-            choice_question = input(f'Please choice a question you would like to edit')
-            if choice not in range(1, len(questions)):
-                raise WrongValueError
 
-        except WrongValueError:
-            print(f'That was not an option, please try again')
-            continue
-        except ValueError:
-            print(f'That was not an integer')
-            continue
+    while True:
+        incrementer = 0
+        incrementer2 = 1
+        inc = 1
+        for i in questions:
+            print(f'Q{incrementer + 1}: {questions[incrementer].body}')
+            for j in i.answers:
+                print(f'{inc}. {j.body} ---> {j.reward}')
+                inc = inc + 1
+            incrementer = incrementer + 1
+            print()
+            print('-' * 40)
+            print()
+        while True:
+            try:
+                choice_question = int(input(f'Please choose a question you would like to edit'))
+                if choice not in range(1, len(questions)):
+                    raise WrongValueError
+
+            except WrongValueError:
+                print(f'That was not an option, please try again')
+                continue
+            except ValueError:
+                print(f'That was not an integer')
+                continue
+            else:
+                break
+
+        clear_console()
+        inc = 1
+        print(f'{questions[choice_question - 1].body}')
+        for i in questions[choice_question - 1].answers:
+            print(f'{inc}. {i.body} ---> {i.reward}')
+
+        while True:
+            try:
+                choice_edit = str(input(f'Would you like to edit the question or answers? (q/a)').lower())
+                if choice_edit not in ('a', 'q'):
+                    raise WrongValueError
+
+            except WrongValueError:
+                print(f'That was not an option, please try again')
+                continue
+            except ValueError:
+                print(f'That was not an string, try again')
+                continue
+            else:
+                break
+
+        if choice_edit == 'a':
+            print(f'pressed a')
         else:
-            break
+            print(f'pressed q')
+
+        #TODO: Setup editing for a specific question and answer
 
 
 def edit_survey():
